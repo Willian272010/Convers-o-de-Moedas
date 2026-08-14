3d47207f1070a592660ad7b22a19cd90c49b6280
Willian Daniel <163948590+Willian272010@users.noreply.github.com>
2026-08-13 21:51:49 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+async function convert() {
+    const value = Number(
+        document.getElementById("value").value
+    );
+
+    const currency =
+        document.getElementById("currency").value;
+
+    const result =
+        document.getElementById("result");
+
+    if (value <= 0) {
+        result.textContent = "Digite um valor válido.";
+        return;
+    }
+
+    try {
+        const response = await fetch(
+            `/api/converter?value=${value}&currency=${currency}`
+        );
+
+        const data = await response.json();
+
+        if (!response.ok) {
+            result.textContent = data.error;
+            return;
+        }
+
+        if (currency === "usd") {
+            result.textContent =
+                `US$ ${data.result.toFixed(2)}`;
+        } else if (currency === "eur") {
+            result.textContent =
+                `€ ${data.result.toFixed(2)}`;
+        }
+    } catch (error) {
+        console.error(error);
+        result.textContent =
+            "Erro ao conectar com o servidor.";
+    }
+}
